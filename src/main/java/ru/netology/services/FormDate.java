13902e8d6aa7ce219e92f48dc98a95d7adae4087
Evgenia Reis <evgenia.kgz@gmail.com>
2023-04-03 15:00:59 +0600

    new files

---
+++ src/main/java/ru/netology/services/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.services;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/services/Main.java
@@ -0,0 +1,19 @@
+package ru.netology.services;
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+
+        post.name = "Ivan";
+        post.passport = "444№44444444";
+        post.patronymic = "Иванов";
+        post.phone = "+7(999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = "true";
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+        System.out.println(post.subscription);
+    }
+}
