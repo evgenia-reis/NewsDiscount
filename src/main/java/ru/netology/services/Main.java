4b713568ae5464a8e80ca2b67802cc486e25bae7
Evgenia Reis <75322442+evgenia-reis@users.noreply.github.com>
2023-04-11 15:13:35 +0600

    Update Main.java

--- src/main/java/ru/netology/services/Main.java
+++ src/main/java/ru/netology/services/Main.java
@@ -9,7 +9,7 @@ public class Main {
         post.patronymic = "Иванов";
         post.phone = "+7(999)-999-99-99";
         post.surname = "Иванов";
-        post.subscription = "true";
+        post.subscription = true;
         post.birthday.day = 13;
         post.birthday.month = 6;
         post.birthday.year = 1999;
